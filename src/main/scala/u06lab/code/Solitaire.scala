34a84b9126601b699502a4664c9f6e1335436f0b
Razvan Vasile <razvanflorian.vasile@studio.unibo.it>
2023-04-19 16:24:33 +0200

    refactor

--- src/main/scala/u06lab/code/Solitaire.scala
+++ src/main/scala/u06lab/code/Solitaire.scala
@@ -8,7 +8,10 @@ object Solitaire extends App:
       private def +(q: Position): Position = (p._1 + q._1, p._2  + q._2)
       private def inside(board: Position): Boolean = p._1 >= 0 && p._1 < board._1 && p._2 >= 0 && p._2 < board._2
 
-
+    extension (arr: Array[Int])
+      private def at(p: Position): Int =
+        val index = p._1 * width + p._2
+        arr(index)
 
     private type Position = (Int, Int)
     private val moves = List((-3, 0), (-2, 2), (0, 3), (2, 2), (3, 0), (2, -2), (0, -3), (-2, -2))
@@ -26,34 +29,33 @@ object Solitaire extends App:
 
 
     private var sol: Int = 0
-    private def findSolution(campo: Array[Int], curMove: Position): Unit =
+    private def findSolution(campo: Array[Int], current: Position): Unit =
 
-      var next:Int = 0
       // 8 possible moves
       for (i <- 0 until 8)
 
         // calculate next cell to place number
-        val nextMove = curMove + moves(i)
+        val nextMove = current + moves(i)
         if(nextMove.inside(height, width))
           val next = nextMove._1 * width + nextMove._2
-          val cur = curMove._1 * width + curMove._2
+          val cur = current._1 * width + current._2
 
-          if (campo(next) == 0)  // is currently empty
+          if (campo.at(nextMove) == 0)  // is currently empty
 
             campo(next) = campo(cur) + 1
 
             if campo(next) < toPlace then
               findSolution(campo, nextMove)
             else
-              sol = sol + 1
-              println(render((campo, sol)))
+              println(render((campo)))
 
             campo(next) = 0
 
 
-    def render(si: (Array[Int], Int)): String =
-      println(s"sol ${si._2}")
-      val reversed = si._1.toSeq
+    def render(si: Array[Int]): String =
+      sol = sol + 1
+      println(s"sol ${sol}")
+      val reversed = si.toSeq
       var arr = for i <- 1 to toPlace
                     number = reversed.indexOf(i)
                     yield if number >= 0 then (number / width, number % width) else (-1, -1)
